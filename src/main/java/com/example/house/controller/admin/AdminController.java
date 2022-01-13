7e7463bc14a5abdfd4ad9dd79ba015b17ded2234
qiaowan <30237108+qiaowanw@163.com>
2022-01-13 13:39:12 +0800

    change-1.13

--- src/main/java/com/example/house/controller/admin/AdminController.java
+++ src/main/java/com/example/house/controller/admin/AdminController.java
@@ -1,13 +1,68 @@
 package com.example.house.controller.admin;
 
+import com.example.house.base.ApiResponse;
+import com.example.house.dto.QiNiuPutRet;
+import com.example.house.service.house.IQiNiuService;
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class AdminController {
+    @Autowired
+    private IQiNiuService qiNiuService;
+    @Autowired
+    private Gson gson;
+
     @RequestMapping("/admin/login")
     public String adminLoginPage() {
         return "admin/login";
     }
+
+    @GetMapping("admin/add/house")
+    public String addHousePage() {
+        return "admin/house-add";
+    }
+
+    @PostMapping(value = "admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+
+        String fileName = file.getOriginalFilename();
+
+        try {
+            InputStream inputStream = file.getInputStream();
+            Response response = qiNiuService.uploadFile(inputStream);
+            if (response.isOK()) {
+                QiNiuPutRet ret = gson.fromJson(response.bodyString(), QiNiuPutRet.class);
+                return ApiResponse.ofSuccess(ret);
+            } else {
+                return ApiResponse.ofMessage(response.statusCode, response.getInfo());
+            }
+
+        } catch (QiniuException e) {
+            Response response = e.response;
+            try {
+                return ApiResponse.ofMessage(response.statusCode, response.bodyString());
+            } catch (QiniuException e1) {
+                e1.printStackTrace();
+                return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (IOException e) {
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
---
+++ src/test/java/com/example/house/service/house/impl/QiNiuServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.example.house.service.house.impl;
+
+import com.example.house.service.house.IQiNiuService;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class QiNiuServiceImplTest {
+    @Autowired
+    private IQiNiuService qiNiuService;
+
+    @Test
+    void testUploadFile() {
+        String fileName = "src/1.jpg";
+        File file = new File(fileName);
+
+        try {
+            Response response = qiNiuService.uploadFile(file);
+            System.out.println(response.isOK());
+        } catch (QiniuException e) {
+            e.printStackTrace();
+        }
+    }
+}
